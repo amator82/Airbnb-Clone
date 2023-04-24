9f1341d230c40363d143773e4ce32f6aa7a158c1
amator82 <ilyakorckishko@gmail.com>
2023-04-24 17:41:44 +0300

    enviroment setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className='text-rose-500 text-2xl p-50'>
+            Hello airbnb
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        </div>
+    )
+}
