5a4efd45aba406afdd75e95595b7d6bf08ca629f
amator82 <ilyakorckishko@gmail.com>
2023-04-25 14:07:16 +0300

    add navbar ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import { Nunito } from 'next/font/google'
+import { Nunito } from 'next/font/google';
 
-import './globals.css'
+import Navbar from './components/navbar/Navbar';
 
-const font = Nunito({ subsets: ['latin'] })
+import './globals.css';
+
+const font = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
-    title: 'Airbnb',
-    description: 'Airbnb clone'
-}
+	title: 'Airbnb',
+	description: 'Airbnb clone'
+};
 
 export default function RootLayout({
-    children
+	children
 }: {
-    children: React.ReactNode
+	children: React.ReactNode;
 }) {
-    return (
-        <html lang='en'>
-            <body className={font.className}>{children}</body>
-        </html>
-    )
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
 }
