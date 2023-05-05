d51110b3c8f7b7cc810d463537ed847ce5910939
amator82 <ilyakorckishko@gmail.com>
2023-05-05 16:52:51 +0300

    create Image upload and Cloudinary CDN

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import Heading from '../Heading';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 import { categories } from '../navbar/Categories';
 
 import useRentModal from '@/app/hooks/useRentModal';
@@ -55,6 +56,7 @@ const RentModal = () => {
 	const guestCount = watch('guestCount');
 	const roomCount = watch('roomCount');
 	const bathroomCount = watch('bathroomCount');
+	const imageSrc = watch('imageSrc');
 
 	const Map = useMemo(
 		() =>
@@ -161,6 +163,21 @@ const RentModal = () => {
 					subtitle='How many bathrooms do you have?'
 					value={bathroomCount}
 					onChange={(value) => setCustomValue('bathroomCount', value)}
+				/>
+			</div>
+		);
+	}
+
+	if (step === STEPS.IMAGES) {
+		bodyContent = (
+			<div className='flex flex-col gap-8'>
+				<Heading
+					title='Add a photo of your place'
+					subtitle='Show guests what your place looks like!'
+				/>
+				<ImageUpload
+					onChange={(value) => setCustomValue('imageSrc', value)}
+					value={imageSrc}
 				/>
 			</div>
 		);
